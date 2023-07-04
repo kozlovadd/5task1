d84a988319151449a4593b99b002a11321aa5195
kozlovadd <kdd_96@mail.ru>
2023-07-04 21:06:04 +0300

    three commit

--- src/test/java/org/example/FreelancerServiceTest.java
+++ src/test/java/org/example/FreelancerServiceTest.java
@@ -9,7 +9,8 @@ class FreelancerServiceTest {
     @Test
     public void testCalculate() {
         FreelancerService service = new FreelancerService();
-        int actual = service.calculate(income:10_000, expenses:3_000, threshold:20_000);
+
+        int actual = service.calculate(10_000, 3_000, 20_000);
         int expected = 3;
 
         Assertions.assertEquals(expected, actual);
@@ -18,7 +19,7 @@ class FreelancerServiceTest {
     @Test
     public void testCalculate2() {
         FreelancerService service = new FreelancerService();
-        int actual = service.calculate(income:100_000, expenses:60_000, threshold:150_000);
+        int actual = service.calculate(100_000, 60_000, 150_000);
         int expected = 2;
 
         Assertions.assertEquals(expected, actual);
